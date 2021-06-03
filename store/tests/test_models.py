a88f5e47f817694e1827b7726551341855f1ee97
Marek Gniady <marek.j.gniady@gmail.com>
2021-06-03 13:12:49 +0200

    initial commit

---
+++ store/tests/test_models.py
@@ -0,0 +1,21 @@
+from django.test import TestCase
+
+from store.models import Category, Product
+
+class TestCategoriesModel(TestCase):
+
+    def setUp(self):
+        self.data1 = Category.objects.create(name='django', slug='django')
+
+    def test_category_model_entry(self):
+        data = self.data1
+        self.assertTrue(isinstance(data, Category))
+
+    def test_category_model_entry(self):
+        data = self.data1
+        self.assertEqual(str(data), 'django')
+
+
+class TestProductsModel(TestCase):
+    def setUp(self):
+        Category.objects.create(name='django', slug='django')
